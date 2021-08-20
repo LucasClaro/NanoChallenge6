1b4764dd9105a67d0512b32b5aef3a0f3875c14e
Gabriel Rodrigues <gabrielrsenac@gmail.com>
2021-08-20 15:37:48 -0300

    app clip e associated domains adicionados

---
+++ NanoChallenge6/NanoChallenge6Clip/ContentViewClip.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NanoChallenge6Clip
+//
+//  Created by Gabriel Rodrigues da Silva on 20/08/21.
+//
+
+import SwiftUI
+
+struct ContentViewClip: View {
+    var body: some View {
+        ContentView()
+    }
+}
+
+struct ContentViewClip_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentViewClip()
+    }
+}
