29ec802f9c905c9167d0d18ede5cda304e6dc9b5
Lucas Claro <lucasbrclaro@gmail.com>
2021-08-23 15:25:08 -0300

    Mais visual e teste com searchbar

--- NanoChallenge6/NanoChallenge6/ContentView.swift
+++ NanoChallenge6/NanoChallenge6/ContentView.swift
@@ -9,11 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var guia : GuiaAberta = .Home
-    @State var mostrandoItem : Bool = false
-    @State var produtoSelecionado : String? = nil
     @State var searchText : String = ""
-    @State private var isEditing = false
+    @State private var showCancelButton: Bool = false
     
     let produtos = ["ProdutoA","ProdutoB","ProdutoComNomeGradePraTestar","ProdutoD","ProdutoE","ProdutoF","ProdutoG", "ProdutoH"]
     let columns = [
@@ -22,16 +19,6 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        TabView (selection: $guia){
-            Home().tabItem { Image(systemName: "house") }
-                .tag(GuiaAberta.Home)
-            Guia2().tabItem { Image(systemName: "stop") }
-                .tag(GuiaAberta.Guia2)
-        }
-    }
-    
-    func Home() -> some View {
-        
         VStack {
             
             SearchBar()
@@ -49,70 +36,90 @@ struct ContentView: View {
                     }
                         .padding()
                     
-                    ScrollView(.vertical) {
-                        LazyVGrid(columns: columns, spacing: 20) {
-                            ForEach (produtos, id: \.self) { produto in
-                                                    
-                                if searchText == "" || produto.contains(searchText) {
-                                    ZStack {
-                                        Image("Teste2")
-                                            .resizable()
-                                            .frame(minWidth: 200, idealWidth: 200, maxWidth: 200, minHeight: 266, idealHeight: 266, maxHeight: 266, alignment: .center)
-                                        VStack {
-                                            Image("Teste").resizable()
-                                                .frame(minWidth: 150, idealWidth: 150, maxWidth: 150, minHeight: 150, idealHeight: 150, maxHeight: 150, alignment: .center)
-                                            Text(produto)
-                                                .multilineTextAlignment(.center)
-                                                .frame(width: 150, alignment: .center)
-                                        }
-                                    }
-                                        .onTapGesture {
-                                            mostrandoItem.toggle()
-                                            produtoSelecionado = produto
-                                        }
-                                        .sheet(isPresented: $mostrandoItem) {
-                                            ItemAberto(aberto: $mostrandoItem, itemAberto: $produtoSelecionado)
-                                        }
-                                }
-                            
-                            }
-                        }
-                    }
+                    ItensList(searchText: $searchText, produtos: produtos)
+                    
                 }//VStack
         }//VStack
-
-    }
-    
-    func Guia2() -> some View {
-        Rectangle()
-            .fill(Color.blue)
-            .frame(width: 150, height: 300)
+            .resignKeyboardOnDragGesture()
     }
     
     func SearchBar() -> some View {
         HStack {
-         
-            TextField("Search ...", text: $searchText)
-                .padding(7)
-                .padding(.horizontal, 25)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .padding(.horizontal, 10)
-                .onTapGesture {
-                    self.isEditing = true
-                }
- 
-            if isEditing {
+            HStack {
+                Image(systemName: "magnifyingglass")
+
+                TextField("Buscar", text: $searchText, onEditingChanged: { isEditing in
+                    self.showCancelButton = true
+                }).foregroundColor(.primary)
+
                 Button(action: {
-                    self.isEditing = false
                     self.searchText = ""
- 
                 }) {
-                    Text("Cancel")
+                    Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
                 }
-                .padding(.trailing, 10)
-                .transition(.move(edge: .trailing))
-                .animation(.default)
+            }
+            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+
+            if showCancelButton  {
+                Button("Cancel") {
+                        UIApplication.shared.endEditing(true) // this must be placed before the other commands here
+                        self.searchText = ""
+                        self.showCancelButton = false
+                }
+                .foregroundColor(Color(.systemBlue))
+            }
+        }
+            .padding(.horizontal)
+            .navigationBarHidden(showCancelButton)
+    }
+}
+
+struct ItensList: View {
+    
+    @State var mostrandoItem : Bool = false
+    @State var produtoSelecionado : String? = nil
+    
+    @Binding var searchText : String
+    var produtos : [String]
+    
+    let columns = [
+        GridItem(.flexible(minimum: 40), spacing: 0),
+        GridItem(.flexible(minimum: 40))
+    ]
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach (produtos, id: \.self) { produto in
+                                        
+                    if searchText == "" || produto.contains(searchText) {
+                        ZStack {
+                            Image("card")
+                                .resizable()
+                                .frame(minWidth: 200, idealWidth: 200, maxWidth: 200, minHeight: 261, idealHeight: 261, maxHeight: 261, alignment: .center)
+                            VStack {
+                                Image("Teste").resizable()
+                                    .frame(minWidth: 150, idealWidth: 150, maxWidth: 150, minHeight: 150, idealHeight: 150, maxHeight: 150, alignment: .center)
+                                Text(produto)
+                                    .font(.custom("NewYork-Black", size: 18))
+                                    .multilineTextAlignment(.center)
+                                    .frame(width: 150, alignment: .center)
+                                    
+                            }
+                        }
+                            .onTapGesture {
+                                mostrandoItem.toggle()
+                                produtoSelecionado = produto
+                            }
+                            .sheet(isPresented: $mostrandoItem) {
+                                ItemAberto(aberto: $mostrandoItem, itemAberto: $produtoSelecionado)
+                            }
+                    }
+                
+                }
             }
         }
     }
@@ -138,6 +145,30 @@ enum GuiaAberta : Hashable {
     case Home
     case Guia2
     case Guia3
+}
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
